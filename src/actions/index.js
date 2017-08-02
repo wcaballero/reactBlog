0deb422e5b54a22b932e4dc824bf01291c837cad
William Caballero <wcaballero4@gmail.com>
2017-08-02 14:57:44 -0500

    updated root url to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api/posts';
 const API_KEY = '?key=williamcaballero';
 
 // action creator to fetch posts
